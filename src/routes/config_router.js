65c44274b1277689f665fd221da57cdc6dbcb023
Thanh <trmthanh220895@gmail.com>
2024-04-14 03:19:44 +0700

    update

---
+++ src/routes/config_router.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route, useRoutes } from "react-router-dom";
+// import Header from '../components/Header';
+import ErrorPage from '../pages/ErrorPage';
+import HomePage from '../pages/HomePage';
+// import ManagementPage from '../pages/ManagementPage';
+// import DetailPage from '../pages/DetailPage';
+// import TheaterPage from '../pages/TheaterPage';
+// import ListSeats from '../components/ListSeats';
+
+function useRouteCustom() {
+
+    const routes=useRoutes([
+      {
+        path:"/",
+        element:<HomePage/>,
+      },
+    //   {
+    //     path:"/detail/:tenPhim",
+    //     element:<DetailPage/>
+    //   },
+    //   {
+    //     path:"/theater",
+    //     element:<TheaterPage/>,
+    //     children:[
+    //       {
+    //         path:"book_ticket/:maLichChieu",
+    //         element:<ListSeats/>
+    //       }
+    //   ]
+    //   },
+    //   {
+    //     path:"/management",
+    //     element:<ManagementPage/>,
+    //   },
+      // {
+      // path:"/detail",
+      // element:<Detail/>,
+      // children:[
+      //     {
+      //       path:"dtdd",
+      //       element:<DetailProduct/>
+      //     },
+      //     {
+      //       path:"mt",
+      //       element:<DetailProduct/>
+      //     }
+      // ]
+      // },
+      {
+        path:"*",
+        element:<ErrorPage/>
+      }
+  ])
+  return  routes;
+}
+
+export default useRouteCustom
